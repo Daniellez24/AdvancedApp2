ff84bacd12c5882b6302e646d1f0dd94b4f50160
Tamir Abutbul <1tamir198@gmail.com>
2022-12-01 13:12:49 +0200

    Adding starter files of the server

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,1 +1,17 @@
-console.log("hello");
+var express = require("express");
+var app = express();
+
+app.get("/products", function (req, res) {
+  res.send("Hey, I am get response");
+});
+
+app.post("/addProduct", function (req, res) {
+  res.send("Hey, I am post response");
+});
+
+const server = app.listen(8081, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log("App listening at http://localhost:8081/", host, port);
+});
