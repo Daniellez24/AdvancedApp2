79ced9faaa6eb8d2eab13dac1c1ec1accf372f8c
Tamir Abutbul <1tamir198@gmail.com>
2022-12-01 17:43:44 +0200

    Change Rout - find all users (this is bugged need to get fixed)

--- BackEnd/controllers/productController.js
+++ BackEnd/controllers/productController.js
@@ -1,5 +1,9 @@
+import { ProductModel } from "../schemas/product.js";
+
 export async function getAllProducts(req, res) {
-  res.send("get all products from here");
+  //TODO check why this is an error when returning allUsers
+  let allUsers = ProductModel.find({}, function (err, file) {});
+  res.send("Getting all the users");
 }
 
 export async function addProducts(req, res) {
