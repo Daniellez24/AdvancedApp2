c9594c200a9fad3a5d2f8ba342255540171c747f
Tamir Abutbul <1tamir198@gmail.com>
2022-12-01 17:38:31 +0200

    Delete dead code

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,12 +2,11 @@ import express from "express";
 var app = express();
 
 import productRouts from "./routes/product.js";
+import { formDbConnection } from "./services/db.js";
 
+formDbConnection();
 app.use(productRouts);
 
-const server = app.listen(8081, function () {
-  const host = server.address().address;
-  const port = server.address().port;
-
-  console.log("App listening at http://localhost:8081/", host, port);
+app.listen(8081, function () {
+  console.log("App listening at http://localhost:8081/");
 });
