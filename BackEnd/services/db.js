e4326e7a02b996971606afa6c4fd269a6144f2ad
Tamir Abutbul <1tamir198@gmail.com>
2022-12-03 18:09:00 +0200

    Change mongo connection string

--- BackEnd/services/db.js
+++ BackEnd/services/db.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 export function formDbConnection() {
   try {
     mongoose.connect(
-      "mongodb+srv://tamir:tamir123456@cluster0.qgr0sfw.mongodb.net/?retryWrites=true&w=majority",
-      { useNewUrlParser: true, useUnifiedTopology: true }
+      "mongodb+srv://tamir:tamir123456@cluster0.qgr0sfw.mongodb.net/Cluster0"
     );
   } catch (error) {
     return `Error connecting to Mongo ${error}`;
