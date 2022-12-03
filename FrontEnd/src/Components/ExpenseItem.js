3b64efaf695127c8572e2581dcb1e1fc0d3eb1bc
Daniellez24 <daniellez1123@gmail.com>
2022-12-03 19:57:33 +0200

    add buttons

--- FrontEnd/src/Components/ExpenseItem.js
+++ FrontEnd/src/Components/ExpenseItem.js
@@ -1,14 +1,31 @@
 import './ExpenseItem.css'
+import {useRef, useEffect} from 'react';
+
 
 export function ExpenseItem({name, price, img}){
 
+    // cartList = {}
+
+    function buttonClicked(){
+        alert("Button clicked")
+        // cartItem = {
+        //     "amount":"3", 
+        //     "name":"ProductName",
+        //     "price":"20",
+        //     "imgUrl":"This is a dummy url"
+        // }
+    }
+
     return (
+
         <div className="container">
             <div> 
                 <p className='productName'>{name}</p>
                 <p className='price'>{price}</p>
+                <button class= "cartbtn" onClick={buttonClicked} >Add to cart</button>
                 </div>
             <img src={img} alt="This is a cool image"/>
+            
         </div>
     )
 }
