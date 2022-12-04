e9aa90cbdc49c15050b94d758198781fe49101f3
Daniellez24 <daniellez1123@gmail.com>
2022-12-04 18:45:09 +0200

    go to cart

--- FrontEnd/src/pages/shop/App.js
+++ FrontEnd/src/pages/shop/App.js
@@ -1,17 +1,22 @@
+import * as React from 'react';
 import { ExpenseItem } from "../../Components/ExpenseItem";
 import { useProducts } from "./useProducts";
+import { useNavigate } from 'react-router-dom';
 
 import "./App.css";
 
 function App() {
   const products = useProducts();
+  const navigate = useNavigate();
 
   return (
     <>
       <h1>Our shop</h1>
+      <button className='goToCart' onClick={() => navigate('../cart/')}>Go to cart</button>
       {products &&
         products.map((item, index) => {
           return (
+            <div>
             <ExpenseItem
               key={item._id}
               name={item.name}
@@ -19,6 +24,7 @@ function App() {
               price={item.price}
               index={index}
             />
+              </div>
           );
         })}
     </>
