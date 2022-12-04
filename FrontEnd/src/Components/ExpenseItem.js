264e4809e5c867449d6714a29c70919a1698d0e6
Daniellez24 <daniellez1123@gmail.com>
2022-12-04 17:35:38 +0200

    formatted

--- FrontEnd/src/Components/ExpenseItem.js
+++ FrontEnd/src/Components/ExpenseItem.js
@@ -1,31 +1,26 @@
-import './ExpenseItem.css'
-import {useRef, useEffect} from 'react';
+import "./ExpenseItem.css";
+import { useRef, useEffect } from "react";
 
+export function ExpenseItem({ name, price, img, index }) {
+  function increaseAmount() {
+    let list = JSON.parse(localStorage.getItem("items"));
+    list[index].amount += 1;
+    localStorage.setItem("items", JSON.stringify(list));
+    console.log(JSON.parse(localStorage.getItem("items")));
 
-export function ExpenseItem({name, price, img, index}){
-    
-    
+    alert("Item added to cart!");
+  }
 
-    function increaseAmount(){
-        let list = JSON.parse(localStorage.getItem("items")); 
-        list[index].amount += 1;
-        localStorage.setItem('items', JSON.stringify(list));
-        console.log(JSON.parse(localStorage.getItem("items")))
-        
-        alert("Item add to cart!");
-
-    }
-
-    return (
-
-        <div className="container">
-            <div> 
-                <p className='productName'>{name}</p>
-                <p className='price'>{price}</p>
-                <button className= "cartbtn" onClick={increaseAmount} >Add to cart</button>
-                </div>
-            <img src={img} alt="This is a cool image"/>
-            
-        </div>
-    )
+  return (
+    <div className="container">
+      <div>
+        <p className="productName">{name}</p>
+        <p className="price">{price}</p>
+        <button className="cartbtn" onClick={increaseAmount}>
+          Add to cart
+        </button>
+      </div>
+      <img src={img} alt="This is a cool image" />
+    </div>
+  );
 }
