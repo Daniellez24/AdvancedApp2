f1a4348e1631260ed544ebc4b90fad2cf0ef90b1
Tamir <1tamir198@gmail.com>
2022-12-05 14:40:59 +0200

    Display cart items from local storage

--- FrontEnd/src/pages/cart/cart.js
+++ FrontEnd/src/pages/cart/cart.js
@@ -1,12 +1,36 @@
 import React from "react";
 
-import './cart.css'
+import "./cart.css";
 
 export const Cart = () => {
+  let cartItems = JSON.parse(localStorage.getItem("products")).filter(
+    (item) => item.amount > 0
+  );
+
+  let totalPrice = 0;
+
+  console.log(cartItems);
+
   return (
     <div className="cart__container">
       <h1>This is the cart</h1>
       <p> In here you can see everything in your cart</p>
+      <div className="cart__items">
+        {cartItems.length > 0 &&
+          cartItems.map((item) => {
+            totalPrice += item.price * item.amount;
+            return (
+              <div>
+                <p>{`Product name: ${item.name}`}</p>
+                <p>{`Total price: ${item.amount} X ${item.price}$ = ${
+                  item.amount * item.price
+                }$`}</p>
+              </div>
+            );
+          })}
+      </div>
+
+      <p>Total Cart Price is {totalPrice}$</p>
     </div>
   );
 };
